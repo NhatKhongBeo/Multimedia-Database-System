29318554052eb3c8cb0539910886cfae7543e1f6
Huy Nup <minecraftvipking137@gmail.com>
2024-04-28 16:42:58 +0700

    Co-authored-by: Nhatbotuoi <109586378+Nhatbotuoi@users.noreply.github.com>

---
+++ MDS/cluster.py
@@ -0,0 +1,124 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import shutil
+from sklearn.cluster import KMeans
+import feature
+
+
+def kmeans_1(X, list_image_path, num_cluster=11):
+
+    kmeans = KMeans(n_clusters=num_cluster,random_state =0, n_init=10)
+
+    X = np.array(X)
+
+    kmeans.fit(X)
+
+    pred_label = kmeans.predict(X)
+
+
+    output_dir = 'MDS\kmeans1'
+
+    # Kiểm tra xem thư mục tồn tại chưa, nếu không, tạo mới
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Tạo đường dẫn đến file
+    output_file = os.path.join(output_dir, 'cluster_labels.txt')
+
+
+    # Mở file để ghi dữ liệu
+    with open(output_file, 'w') as file:
+        # Ghi nhãn của từng điểm dữ liệu
+        np.savetxt(file, kmeans.labels_, fmt='%d')
+
+
+    output_file = os.path.join(output_dir, 'cluster_center.txt')
+
+    with open(output_file, 'w') as file:
+        # Ghi trung tâm của các cụm
+
+        np.savetxt(file, kmeans.cluster_centers_)
+
+
+    dict = {}
+
+    for i in range(len(list_image_path)):
+        dict[list_image_path[i]] = pred_label[i]
+
+    for i in range(num_cluster):
+        folder_name = str(i)
+        os.mkdir(os.path.join('MDS\kmeans1', folder_name))
+
+    for key, value in dict.items():
+        for dirname, child_folders, filenames in os.walk('kmeans1'):
+            for folder in child_folders:
+                if str(value) == folder:
+                    shutil.copy(key, os.path.join(dirname, folder))
+    
+    metadata = {}
+    for i in range(len(X)):
+        metadata[tuple(X[i])] = str(pred_label[i])
+    
+    return metadata
+    
+
+def kmeans_2(folder_path, mod):
+    images = []
+    list_image_path = []
+    for filename in os.listdir(folder_path):
+        # print(folder_path)
+        image_path = os.path.join(folder_path, filename)
+        # print(image_path)
+        list_image_path.append(image_path)
+        image = cv2.imread(image_path)
+        # print(image)
+        avg_HSV= feature.average_HSV(image)
+        images.append(avg_HSV)
+    
+    kmeans_second = KMeans(n_clusters=mod,random_state =0, n_init=10)
+    kmeans_second.fit(np.array(images))
+    label = kmeans_second.predict(images)
+        
+    dict = {}
+    for i in range((len(list_image_path))):
+        dict[list_image_path[i]] = label[i]
+    
+
+    for i in range(mod):
+        folder_name =  str(i)
+        os.mkdir(os.path.join(folder_path, folder_name))
+
+    for key, value in dict.items():
+        for dirname, child_folders, _ in os.walk(folder_path):
+            for folder in child_folders:
+                if str(value) == folder:
+                    shutil.move(key, os.path.join(dirname, folder))
+
+    # Tạo đường dẫn đến file
+    output_file = os.path.join(folder_path, 'cluster_labels.txt')
+
+
+    # Mở file để ghi dữ liệu
+    with open(output_file, 'w') as file:
+        # Ghi nhãn của từng điểm dữ liệu
+        np.savetxt(file, kmeans_second.labels_, fmt='%d')
+
+
+    output_file = os.path.join(folder_path, 'cluster_center.txt')
+
+    with open(output_file, 'w') as file:
+        # Ghi trung tâm của các cụm
+
+        np.savetxt(file, kmeans_second.cluster_centers_)
+    
+    last_folder = os.path.split(folder_path)[-1]
+
+    metadata = {}
+    for i in range(len(images)):
+        metadata[tuple(images[i])] = f'{last_folder}.{label[i]}'
+    
+    return metadata
+
+
---
+++ MDS/feature.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+import pickle
+
+def average_BGR(image):
+    avg_blue = image[:,:,0].mean()
+    avg_green = image[:,:,1].mean()
+    avg_red = image[:,:,2].mean()
+
+    return [avg_blue,avg_green,avg_red]
+
+def average_HSV(image):
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    avg_hue = image[:,:,0].mean()
+    avg_saturation = image[:,:,1].mean()
+    avg_value = image[:,:,2].mean()
+
+    return avg_hue, avg_saturation, avg_value
+
+def cover_to_gray(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return gray
+
+def SIFT(image):
+    sift = cv2.SIFT_create()
+    _, descriptors = sift.detectAndCompute(image, None)
+    return  descriptors
+
+def kmeans_bow(all_descriptors, num_clusters):
+
+    bow_dict = []
+    kmeans = KMeans(n_clusters=num_clusters, verbose = 1).fit(all_descriptors)
+    bow_dict = kmeans.cluster_centers_
+    
+    return bow_dict
+
+def create_features_bow(single_image_descriptors, BoW, num_clusters):
+    feature = np.array([0] * num_clusters)
+    if single_image_descriptors is not None:
+        distance = cdist(single_image_descriptors, BoW, metric='euclidean')
+        argmin = np.argmin(distance, axis=1)
+        for j in argmin:
+            feature[j] += 1
+    return feature
+
+
