f282e0329d935eaa173fecc7177474a254228eef
Huy Nup <minecraftvipking137@gmail.com>
2024-06-08 16:12:27 +0700

    updata

---
+++ cluster.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import shutil
+from sklearn.cluster import KMeans
+import feature
+
+def kmeans_1(X, list_image_path, num_cluster=11):
+
+    kmeans = KMeans(n_clusters=num_cluster, random_state=0)
+
+    X = np.array(X)
+
+    kmeans.fit(X)
+
+    pred_label = kmeans.predict(X)
+
+    output_dir = "D:\\Nhat\\term_8\\MDS\\src\\kmeans"
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    dict = {}
+
+    for i in range(len(list_image_path)):
+        dict[list_image_path[i]] = pred_label[i]
+
+    for i in range(num_cluster):
+        folder_name = str(i)
+        os.mkdir(
+            os.path.join(
+                "D:\\Nhat\\term_8\\MDS\\src\\kmeans", folder_name
+            )
+        )
+
+    for key, value in dict.items():
+        for dirname, child_folders, filenames in os.walk("kmeans"):
+            for folder in child_folders:
+                if str(value) == folder:
+                    shutil.copy(key, os.path.join(dirname, folder))
+
+    return kmeans.labels_, kmeans.cluster_centers_
+
+
+def kmeans_2(folder_path, mod):
+    images = []
+    list_image_path = []
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+            image_path = os.path.join(folder_path, filename)
+            list_image_path.append(image_path)
+            image = cv2.imread(image_path)
+            avg_HSV = feature.average_HSV_simple(image)
+            images.append(avg_HSV)
+
+    kmeans_second = KMeans(n_clusters=mod, random_state=0)
+    kmeans_second.fit(np.array(images))
+    labels = kmeans_second.predict(images)
+
+    dict = {}
+    for i in range((len(list_image_path))):
+        dict[list_image_path[i]] = labels[i]
+
+    for i in range(mod):
+        folder_name = str(i)
+        os.mkdir(os.path.join(folder_path, folder_name))
+
+    for key, value in dict.items():
+        for dirname, child_folders, _ in os.walk(folder_path):
+            for folder in child_folders:
+                if str(value) == folder:
+                    shutil.move(key, os.path.join(dirname, folder))
+
+    # Tạo đường dẫn đến file
+    output_file = os.path.join(folder_path, "cluster_labels.txt")
+
+    # Mở file để ghi dữ liệu
+    with open(output_file, "w") as file:
+        # Ghi nhãn của từng điểm dữ liệu
+        np.savetxt(file, kmeans_second.labels_, fmt="%d")
+
+    output_file = os.path.join(folder_path, "cluster_center.txt")
+
+    with open(output_file, "w") as file:
+        # Ghi trung tâm của các cụm
+
+        np.savetxt(file, kmeans_second.cluster_centers_)
---
+++ metadata.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import feature
+import cluster
+
+ROOT_PATH = "D:\\Nhat\\term_8\\MDS\\src\\Mix"
+NUM_CLUSTER = 11
+
+X = []
+
+list_image = []
+list_image_path = []
+for dirname, _, filenames in os.walk("D:\\Nhat\\term_8\\MDS\\src\\Mix"):
+    for filename in filenames:
+        image_path = os.path.join(dirname, filename)
+        image = cv2.imread(image_path)
+        list_image_path.append(image_path)
+        list_image.append(image)
+        avg_HSV = feature.average_HSV(image)
+        print(avg_HSV)
+        X.append(avg_HSV)
+
+
+
+def clustering():
+
+    props_kmeans_1 = cluster.kmeans_1(X, list_image_path, NUM_CLUSTER)
+    (labels, center_values) = props_kmeans_1
+
+    output_dir = "D:\\Nhat\\term_8\\MDS\\src\\kmeans"
+
+    # Tạo đường dẫn đến file
+    output_file = os.path.join(output_dir, "cluster_labels.txt")
+
+    # Mở file để ghi dữ liệu
+    with open(output_file, "w") as file:
+        # Ghi nhãn của từng điểm dữ liệu
+        np.savetxt(file, labels, fmt="%d")
+
+    output_file = os.path.join(output_dir, "cluster_center.txt")
+
+    with open(output_file, "w") as file:
+        # Ghi trung tâm của các cụm
+
+        np.savetxt(file, center_values)
+
+    # Thực hiện kmeans lần thứ hai
+    if os.path.exists(output_dir):
+        for i in range(NUM_CLUSTER):
+            folder_path = os.path.join(output_dir, str(i))
+            #mod = (len(os.listdir(folder_path)) - 1) // 13
+            mod = (len(os.listdir(folder_path))) / 15
+            if mod > round(mod):
+                mod = round(mod) + 1
+            else:
+                mod = round(mod)
+            if mod > 1:
+                cluster.kmeans_2(folder_path, mod)
+
+
+def metadata():
+
+    for dirname, child_folders, filenames in os.walk("kmeans"):
+
+        if len(child_folders) == 0:
+            list_feature_image = []
+            # Ghi metadata
+            file_name = os.path.join(dirname, "metadata.txt")
+            list_image_feature = feature.get_feature_bow(dirname)
+            if not os.path.exists(file_name):  # Sửa từ 'exits' thành 'exists'
+                # Ghi metadata vào file nếu file không tồn tại
+                with open(file_name, "w") as file:
+                    for key, value in list_image_feature.items():
+                        file.write(f"{key}: {value}\n")
+
+clustering()
+metadata()
